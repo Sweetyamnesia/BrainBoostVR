00efd8759021b183ae3626f7db8046e10f1e0106
Sweetyamnesia <angela.r@live.fr>
2025-09-23 10:12:13 +0200

    feat: implement teleportation detection and prepare event logging

--- BrainBoostVR_Unity/BrainBoostVR/Assets/Scripts/PlayerController.cs
+++ BrainBoostVR_Unity/BrainBoostVR/Assets/Scripts/PlayerController.cs
@@ -6,48 +6,69 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
-	// Variables for logging
+	// Variables for grabbing
 	public XRBaseInteractor leftHandInteractor;
 	public XRBaseInteractor rightHandInteractor;
 	private List<XRGrabInteractable> grabbableObjects;
 
-	// Start is called before the first frame update
+	// Variables for teleportation
+    public TeleportationProvider teleportationProvider;
+    public XROrigin xrOrigin;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
 	void Start()
 	{
-		// Search for all XR Grab Interactable objects in the scene
+		// Subscription to grab/release events
 		grabbableObjects = new List<XRGrabInteractable>(FindObjectsOfType<XRGrabInteractable>());
-
-		// For each object, subscribe to DetectGrab and DetectRelease
 		foreach (var obj in grabbableObjects)
 		{
 			obj.selectEntered.AddListener(DetectGrab);
 			obj.selectExited.AddListener(DetectRelease);
 		}
+
+		// Teleportation event subscription
+        if (teleportationProvider != null)
+        {
+            teleportationProvider.teleportationStarted.AddListener(OnTeleportStart);
+            teleportationProvider.teleportationCompleted.AddListener(OnTeleportEnd);
+        }
 	}
 
+	// Grab detection
 	void DetectGrab(SelectEnterEventArgs args)
 	{
 		EmitEvent("GrabObject");
 		Debug.Log(args.interactableObject.transform.name + " saisi");
 	}
 
+	// Release detection
 	void DetectRelease(SelectExitEventArgs args)
 	{
 		Debug.Log(args.interactableObject.transform.name + " relâché");
 		EmitEvent("ReleaseObject");
 	}
 
+	// Start teleportation
+    void OnTeleportStart(TeleportingEventArgs args)
+    {
+        startPosition = xrOrigin.transform.position;
+        Debug.Log("Téléportation commencée depuis " + startPosition);
+    }
+
+    // End of teleportation
+    void OnTeleportEnd(TeleportingEventArgs args)
+    {
+        endPosition = xrOrigin.transform.position;
+        Debug.Log("Téléportation terminée à " + endPosition);
+        EmitEvent("Teleportation");
+    }
+
+	// Event management
 	void EmitEvent(string eventName)
 	{
 		// Send the event name to the ExerciseManager
 		Debug.Log("Event émis : " + eventName);
-	}
-
-	void DetectTeleport()
-	{
-		// 1. Check if the player is moving or teleporting
-        // 2. Add a log: position + timestamp
-        // 3. Check that teleportation does not go through walls
 	}
 
 	void DetectDoorOpen()
@@ -59,11 +80,9 @@ public class PlayerController : MonoBehaviour
 
 	void LogInteraction()
 	{
-		 // 1. Add a message to the console/log for debugging
-        // 2. Example: “The player picked up item X at 12:34”
+		Debug.Log("[INTERACTION] " + logMessage);
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 
