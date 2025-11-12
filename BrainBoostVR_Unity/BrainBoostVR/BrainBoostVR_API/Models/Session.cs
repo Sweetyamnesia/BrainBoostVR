a545ed17a6ab1064ce9c79f06fdcfbac9df4f0ee
Sweetyamnesia <angela.r@live.fr>
2025-11-12 22:27:10 +0100

    🔹 Update Session model: add FirebaseUID, SessionUid, Pseudo, Score, and Errors fields

--- BrainBoostVR_Unity/BrainBoostVR/BrainBoostVR_API/Models/Session.cs
+++ BrainBoostVR_Unity/BrainBoostVR/BrainBoostVR_API/Models/Session.cs
@@ -1,13 +1,13 @@
-using System;
-
-namespace BrainBoostVR_API.Models
+public class Session
 {
-    public class Session
-    {
-        public int SessionID { get; set; }            // NOT NULL
-        public int UserID { get; set; }               // NOT NULL, FK -> User
-        public DateTime? StartTime { get; set; }      // nullable
-        public DateTime? EndTime { get; set; }        // nullable
-        public float? DurationMinutes { get; set; }   // nullable
-    }
+    public int SessionID { get; set; }
+    public int UserID { get; set; }
+	public string? Pseudo { get; set; } = string.Empty;
+    public string FirebaseUID { get; set; } = string.Empty;  // ajouté
+    public string SessionUid { get; set; } = string.Empty;   // ajouté
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public float? DurationMinutes { get; set; }
+    public int Score { get; set; } = 0;
+    public int Errors { get; set; } = 0;
 }
