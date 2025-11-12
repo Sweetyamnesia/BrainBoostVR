b5e9aed88a0a6e365c923692ca765c11f969d78e
Sweetyamnesia <angela.r@live.fr>
2025-11-12 22:24:48 +0100

    🔹 CreateUserDto: add default empty values for FirebaseUID and Name to prevent null issues

--- BrainBoostVR_Unity/BrainBoostVR/BrainBoostVR_API/Models/CreateUserDto.cs
+++ BrainBoostVR_Unity/BrainBoostVR/BrainBoostVR_API/Models/CreateUserDto.cs
@@ -2,7 +2,7 @@ namespace BrainBoostVR_API.Models
 {
     public class CreateUserDto
     {
-        public string FirebaseUID { get; set; }
-        public string Name { get; set; }
+		public string FirebaseUID { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
     }
 }
